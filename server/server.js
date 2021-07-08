5883417c6acd98d108a775cccbb02e1e9df5c61b
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-08 09:56:37 +0200

    switch naming conventions

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ require('dotenv').config()
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 8080;
-const DATABASE_PORT = process.env.DATABASE_PORT || 27015;
-
+const SERVER_PORT = process.env.SERVER_PORT;
+const DATABASE_PORT = process.env.DATABASE_PORT;
+console.log("Variables loaded from the .env file:");
+console.log(`SERVER_PORT: ${SERVER_PORT}`);
+console.log(`DATABASE_PORT: ${DATABASE_PORT}`);
 mongoose.connect(`mongodb://root:password@localhost:${DATABASE_PORT}/admin?authSource=admin&readPreference=primary&gssapiServiceName=mongodb&appname=MongoDB%20Compass&ssl=false`, 
 { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:true })
 .then(() => {
-    console.log("[+] Connected to the database on port 27017!");
+    console.log(`[+] Connected to the database on port ${DATABASE_PORT}!`);
 })
 .catch(err => {
     console.log(err);
@@ -23,6 +25,6 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use('/api', require('./router/routes.js'))
 
-app.listen(PORT, () => {
-    console.log(`Server running on port :${PORT}`);
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running on port :${SERVER_PORT}`);
 });
